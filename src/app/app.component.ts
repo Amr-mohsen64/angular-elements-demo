81d5bb9560b786ebac433a20394edf8673a82a02
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2023-08-20 20:23:24 +0300

    added project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
+import { AlertComponent } from './alert/alert.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-elements-demo';
+  content!: string | SafeHtml;
+
+  constructor(injector: Injector, domSanitizer: DomSanitizer) {
+    const alertElement = createCustomElement(AlertComponent, {
+      injector: injector,
+    });
+    customElements.define('amr-alert', alertElement);
+    setTimeout(() => {
+      this.content = domSanitizer.bypassSecurityTrustHtml(
+        '<amr-alert message="renderd dianmically"></amr-alert>'
+      );
+    }, 2000);
+  }
 }
